9502fb346c88dd40c306da83691da45019b06195
Andrew <andrew.knox112@gmail.com>
2016-11-02 18:39:57 -0400

    Tested out adding popup windows through JOptionPane and tested the creation and use of threads as well as parsing strings to ints and error checking for the parsing

--- test/TestGUITextInput.java
+++ test/TestGUITextInput.java
@@ -4,66 +4,73 @@ import processing.core.PFont;
 
 import java.awt.*;
 
+import javax.swing.JOptionPane;
+
 /**
- * * File: TestGUITextInput
- * * Author: Ben Kinder
- * * Date Created: 11/1/16
- * * Class: COMP 101
- * * Email: bkinder1@umbc.edu
- * * Description
+ * * File: TestGUITextInput * Author: Ben Kinder * Date Created: 11/1/16 *
+ * Class: COMP 101 * Email: bkinder1@umbc.edu * Description
  */
 public class TestGUITextInput extends PApplet {
-    private ControlP5 test;
-    private String creditHours = "", workHours = "";
-    private Textfield creditHoursField;
-    private Textfield workHoursField;
-    private PFont font;
+	private ControlP5 test;
+	private String creditHours = "", workHours = "";
+	private Textfield creditHoursField;
+	private Textfield workHoursField;
+	private PFont font;
 
-    public static void main(String[] args) {
-        PApplet.main("TestGUITextInput");
-    }
+	public static void main(String[] args) {
+		PApplet.main("TestGUITextInput");
+	}
 
-    public void settings() {
-        size(1024, 768);
-    }
+	public void settings() {
+		size(1024, 768);
+	}
 
-    public void setup() {
-        font = createFont("arial", 16);
-        background(128, 0, 128);
-        test = new ControlP5(this);
-        fill(255);
-        text("Credit Hours: ", 20, 115);
-        creditHoursField = test.addTextfield("creditHours")
-                .setPosition(100, 100)
-                .setSize(200, 20)
-                .setColorBackground(color(255, 255, 255))
-                .setLabelVisible(false)
-                .setLabel("")
-                .setColor(color(0))
-                .setFont(font);
-        text("Work Hours: ", 20, 165);
-        workHoursField = test.addTextfield("workHours")
-                .setPosition(100, 150)
-                .setSize(200, 20)
-                .setColorBackground(color(255, 255, 255))
-                .setLabel("")
-                .setColor(color(0))
-                .setFont(font);
+	public void setup() {
+		Thread textListener = new Thread(new Runnable() {
+			private String creditHoursTemp = creditHours;
 
-        test.addButton("submit").setSize(50, 20).setPosition(900, 700).setLabel("Submit");
-    }
+			public void run() {
+				try {
+					while (true) {
+						if (!creditHoursTemp.equals(creditHours)) {
+							int creditHoursNum = Integer.parseInt(creditHours);
+						}
+						println("Running");
+						Thread.sleep(1000);
+					}
+				} catch (Exception e) {
+					println("Error");
+				}
+			}
+		});
 
-    public void draw() {
-        text(creditHours, 400, 115);
-        text(workHours, 400, 165);
-    }
+		font = createFont("arial", 16);
+		background(128, 0, 128);
+		test = new ControlP5(this);
+		fill(255);
+		text("Credit Hours: ", 20, 115);
+		creditHoursField = test.addTextfield("creditHours").setPosition(100, 100).setSize(200, 20)
+				.setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
+				.setFont(font);
+		text("Work Hours: ", 20, 165);
+		workHoursField = test.addTextfield("workHours").setPosition(100, 150).setSize(200, 20)
+				.setColorBackground(color(255, 255, 255)).setLabel("").setColor(color(0)).setFont(font);
 
-    public void submit() {
-        creditHours = creditHoursField.getText();
-        creditHoursField.clear();
-        workHours = workHoursField.getText();
-        workHoursField.clear();
-    }
+		test.addButton("submit").setSize(50, 20).setPosition(900, 700).setLabel("Submit");
+		textListener.start();
+	}
 
+	public void draw() {
+		text(creditHours, 400, 115);
+		text(workHours, 400, 165);
+	}
+
+	public void submit() {
+		creditHours = creditHoursField.getText();
+		creditHoursField.clear();
+		workHours = workHoursField.getText();
+		workHoursField.clear();
+		JOptionPane.showMessageDialog(null, "Testing");
+	}
 
 }
