79252bb8829434d3068847eda6a7fb981c2e2f1f
DrBananaEsq <aawood129@gmail.com>
2016-11-12 20:04:13 -0500

    you can play it like a video game, added math, weekly calculations, intro calculations, calculations for your life

--- src/MainSimulator.java
+++ src/MainSimulator.java
@@ -27,12 +27,14 @@ public class MainSimulator extends PApplet {
 	private static JFrame gameFrame;
     private ControlP5 cp5;
     private float creditHours, workHours, classTime, studyTime, academicVisit, partyTime, wealthWeekly, wealthTotal,
-                                happyWeekly, happySum, happyAverage, gradeWeekly, gradeSum, gradeAverage,
-                                work, study, classt, aca, party, math;
+                                happyWeekly, happySum, happyAverage, gradeWeekly, gradeSum, gradeAverage;
+
+    private float work, study, classt, aca, party, math; // Temporary variables for math shenanigans
+
     private Textfield creditHoursField, workHoursField, classTimeField, studyTimeField, academicVisitField,
             partyTimeField;
     private PFont font;
-    private int fieldRow1x, fieldRow2x, textRow1x, textRow2x;
+    private int fieldRow1x, fieldRow2x, textRow1x, textRow2x, week;
     private final float BASE_HAPPINESS = 50,
                         BASE_WEALTH = 0,
                         BASE_GRADE_POTENTIAL = 90,
@@ -113,6 +115,7 @@ public class MainSimulator extends PApplet {
     }
 
     public void setup() {
+        firstWeekStats();
         fieldRow1x = 115;
         fieldRow2x = 500;
         textRow1x = 20;
@@ -170,7 +173,8 @@ public class MainSimulator extends PApplet {
                 .setColor(color(0))
                 .setFont(font);
 
-        cp5.addButton("submit").setSize(50, 20).setPosition(900, 700).setLabel("Submit");
+        cp5.addButton("nextWeek").setSize(50, 20).setPosition(900, 700).setLabel("Next Week");
+        cp5.addButton("weekStart").setSize(50, 20).setPosition(900, 500).setLabel("Start Week");
         cp5.addButton("creditSelect").setSize(50,20).setPosition(330,fieldRow1x-15).setLabel("Credits");
     }
 
@@ -182,21 +186,81 @@ public class MainSimulator extends PApplet {
         text("Study Time: ", textRow1x, 265);
         text("Academic Visits: ", textRow2x, 115);
         text("Party Time: ", textRow2x, 165);
+        text("Happiness: " + happyWeekly, 500, 300);
+        text("Weekly Wealth: " + wealthWeekly, 500, 350);
+        text("Weekly Grades: " + gradeWeekly, 500, 400);
+        text("Week: " + week, 500, 450);
+
     }
 
-    public void weeklyReset(){
+    public void firstWeekStats(){
         wealthWeekly = 0;
+        happySum = 0;
+        gradeSum = 0;
+        week = 1;
         happyWeekly = BASE_HAPPINESS;
         gradeWeekly = BASE_GRADE_POTENTIAL;
     }
 
-    public void matheWereDeclared(){
-        happyWeekly = happyWeekly;
+    public boolean weekCounter(){
+        if (week >= 14){
+            return true;
+        }else return false;
+
     }
-    public void mathWereDeclaredAgain(){
+
+    public void weeklyReset(){
         happySum += happyWeekly;
         wealthTotal += wealthWeekly;
         gradeSum += gradeWeekly;
+        wealthWeekly = 0;
+        happyWeekly = BASE_HAPPINESS;
+        gradeWeekly = BASE_GRADE_POTENTIAL;
+        week += 1;
+    }
+
+    public void mathForWeek(){
+        float negative;
+        //credits
+        if(creditHours >= 12){
+            happyWeekly -= 1.25f*creditHours;
+            gradeWeekly -= 1.5f*creditHours;
+            }
+        //work
+        wealthWeekly += (5*workHours);
+        if(workHours >= 21){
+            happyWeekly -= 1*workHours;
+            gradeWeekly -= .5*workHours;
+        } else happyWeekly -= .5*workHours;
+        if(wealthWeekly >= 55){ happyWeekly += 5; }
+        //class time
+        happyWeekly -= 1*classTime;
+        gradeWeekly += .5*classTime;
+        //study
+        gradeWeekly += .5*studyTime;
+        happyWeekly -= .5*studyTime;
+        //academic visit
+        gradeWeekly += .5*academicVisit;
+        happyWeekly -= .5*academicVisit;
+        //Leisure time
+        if(partyTime <= 0) {gradeWeekly -= 50;}
+        gradeWeekly -= 3*partyTime;
+        wealthWeekly -= 6*partyTime;
+        if ((wealthWeekly - 6*partyTime) <= 0 ){
+            negative = (wealthWeekly -= 7*partyTime);
+            wealthTotal += negative;
+            if (wealthTotal <= 0){
+                wealthTotal = 0;
+                System.out.println("YOU BROKE BRUH");
+            }
+            wealthWeekly = 0;
+            happyWeekly = .25f*partyTime;
+        } else happyWeekly += 3*partyTime;
+        // happy check
+        if (happyWeekly <= 0){ happyWeekly = 0;}
+
+
+
 
     }
     private boolean numberCheck() {
@@ -216,6 +280,34 @@ public class MainSimulator extends PApplet {
         } else return false;
     }
 
+
+    //Buttons
+
+    public void weekStart() {
+        study = Float.parseFloat(studyTimeField.getText());
+        if (numberCheck()) {
+            workHours = Float.parseFloat(workHoursField.getText());
+            workHoursField.lock();
+            classTime = Float.parseFloat(classTimeField.getText());
+            classTimeField.lock();
+            studyTime = Float.parseFloat(studyTimeField.getText());
+            studyTimeField.lock();
+            academicVisit = Float.parseFloat(academicVisitField.getText());
+            academicVisitField.lock();
+            partyTime = Float.parseFloat(partyTimeField.getText());
+            partyTimeField.lock();
+            mathForWeek();
+            System.out.println("Credit Hours:\t\t" + creditHours + "\n" +
+                    "Work Hours:\t\t\t" + workHours + "\n" +
+                    "Class Time:\t\t\t" + classTime + "\n" +
+                    "Study Time:\t\t\t" + studyTime + "\n" +
+                    "Academic Visits:\t" + academicVisit + "\n" +
+                    "Leisure Time:\t\t\t" + partyTime);
+        }
+        else System.out.println("you broke it");
+
+
+    }
     public void creditSelect() {
         float cred = Float.parseFloat(creditHoursField.getText());
         if (cred >= MIN_CREDITS && cred <= MAX_CREDITS) {
@@ -224,29 +316,28 @@ public class MainSimulator extends PApplet {
         } else System.out.println("Pick a value between 12 and 24");
     }
 
-    public void submit() {
+    public void nextWeek() {
         study = Float.parseFloat(studyTimeField.getText());
         if (numberCheck()) {
-            workHours = Float.parseFloat(workHoursField.getText());
-            workHoursField.setText("0");
-            classTime = Float.parseFloat(classTimeField.getText());
-            classTimeField.setText("0");
-            studyTime = Float.parseFloat(studyTimeField.getText());
-            studyTimeField.setText("0");
-            academicVisit = Float.parseFloat(academicVisitField.getText());
-            academicVisitField.setText("0");
-            partyTime = Float.parseFloat(partyTimeField.getText());
-            partyTimeField.setText("0");
+            workHoursField.unlock();
+            workHoursField.clear();
+            classTimeField.unlock();
+            classTimeField.clear();
+            studyTimeField.unlock();
+            studyTimeField.clear();
+            academicVisitField.unlock();
+            academicVisitField.clear();
+            partyTimeField.unlock();
+            partyTimeField.clear();
 
-            System.out.println("Credit Hours:\t\t" + creditHours + "\n" +
-                    "Work Hours:\t\t\t" + workHours + "\n" +
-                    "Class Time:\t\t\t" + classTime + "\n" +
-                    "Study Time:\t\t\t" + studyTime + "\n" +
-                    "Academic Visits:\t" + academicVisit + "\n" +
-                    "Leisure Time:\t\t\t" + partyTime);
-            mathWereDeclaredAgain();
+            System.out.println("Total Wealth\t\t\t" + wealthTotal + "\n" +
+                    "The happiness and gp average go here");
+            weeklyReset();
         }
         else System.out.println("is brok");
+        if (weekCounter()){
+            System.out.println("FINAL WEEK");
+        }
     }
 
     public JFrame getFrame(){
