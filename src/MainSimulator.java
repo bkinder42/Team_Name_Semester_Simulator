fe549469301e720a66c48a58f8a8a3405efa71ad
DrBananaEsq <aawood129@gmail.com>
2016-12-12 01:54:19 -0500

    fixed overwriting buttons

--- src/MainSimulator.java
+++ src/MainSimulator.java
@@ -320,11 +320,33 @@ public class MainSimulator extends PApplet {
 		cp5 = new ControlP5(this);
 		fill(255);
 
+        creditHoursField = cp5.addTextfield("creditHours").setPosition(fieldRow1x, 100).setSize(200, 20)
+                .setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
+                .setFont(font);
+        workHoursField = cp5.addTextfield("workHours").setPosition(fieldRow1x, 150).setSize(200, 20)
+                .setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
+                .setFont(font);
+        classTimeField = cp5.addTextfield("classTime").setPosition(fieldRow1x, 200).setSize(200, 20)
+                .setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
+                .setFont(font);
+        studyTimeField = cp5.addTextfield("studyTime").setPosition(fieldRow1x, 250).setSize(200, 20)
+                .setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
+                .setFont(font);
+        academicVisitField = cp5.addTextfield("academicVisit").setPosition(fieldRow2x, 100).setSize(200, 20)
+                .setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
+                .setFont(font);
+        partyTimeField = cp5.addTextfield("partyTime").setPosition(fieldRow2x, 150).setSize(200, 20)
+                .setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
+                .setFont(font);
+
+        cp5.addButton("nextWeek").setSize(50, 20).setPosition(900, 700).setLabel("Next Week");
+        cp5.addButton("weekStart").setSize(50, 20).setPosition(900, 500).setLabel("Start Week");
+        cp5.addButton("creditSelect").setSize(50, 20).setPosition(330, fieldRow1x - 15).setLabel("Credits");
+
 	}
 
 	public void draw() {
 		weeklyRun();
-		ghettoButtonHider();
 	}
 
 	private void regularWeekInfo() {
@@ -356,24 +378,7 @@ public class MainSimulator extends PApplet {
 		text("Academic Time: " + "0 - 30", 200, 560);
 		text("Leisure Time: " + "0 - 60", 200, 575);
 
-		creditHoursField = cp5.addTextfield("creditHours").setPosition(fieldRow1x, 100).setSize(200, 20)
-				.setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
-				.setFont(font);
-		workHoursField = cp5.addTextfield("workHours").setPosition(fieldRow1x, 150).setSize(200, 20)
-				.setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
-				.setFont(font);
-		classTimeField = cp5.addTextfield("classTime").setPosition(fieldRow1x, 200).setSize(200, 20)
-				.setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
-				.setFont(font);
-		studyTimeField = cp5.addTextfield("studyTime").setPosition(fieldRow1x, 250).setSize(200, 20)
-				.setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
-				.setFont(font);
-		academicVisitField = cp5.addTextfield("academicVisit").setPosition(fieldRow2x, 100).setSize(200, 20)
-				.setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
-				.setFont(font);
-		partyTimeField = cp5.addTextfield("partyTime").setPosition(fieldRow2x, 150).setSize(200, 20)
-				.setColorBackground(color(255, 255, 255)).setLabelVisible(false).setLabel("").setColor(color(0))
-				.setFont(font);
+
 	}
 
 	public void firstWeekStats() {
@@ -385,15 +390,7 @@ public class MainSimulator extends PApplet {
 		gradeWeekly = BASE_GRADE_POTENTIAL;
 		soundHappyLvl = happyWeekly;
 	}
-	public void ghettoButtonHider(){
-		if (!(creditHours < 0)){
-			cp5.addButton("creditSelect").setSize(50, 20).setPosition(330, fieldRow1x - 15).setLabel("Credits");
-		}
-		if (!isSchoolOver()){
-			cp5.addButton("nextWeek").setSize(50, 20).setPosition(900, 700).setLabel("Next Week");
-			cp5.addButton("weekStart").setSize(50, 20).setPosition(900, 500).setLabel("Start Week");
-		}
-	}
+
 
 	public synchronized void weeklyRun() {
 		if (!isSchoolOver()) {
